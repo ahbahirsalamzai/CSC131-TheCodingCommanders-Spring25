287ac5810f0aa8bd41ff3c8cdea1dd12de0fa9cf
Mohammad Mustafa <mshams@csus.edu>
2025-04-25 09:48:31 -0700

    Clean: removed node_modules and build from tracking

---
+++ StudyHive/Backend/routes/studentRoutes.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import User from '../models/User.js';
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  try {
+    const students = await User.find({ role: 'student', status: 'active' });
+    res.json(students);
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to fetch students.' });
+  }
+});
+// DELETE a student by ID
+router.delete("/:id", async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.json({ message: "Student deleted" });
+  } catch (err) {
+    res.status(500).json({ message: "Delete failed" });
+  }
+});
+
+export default router;
