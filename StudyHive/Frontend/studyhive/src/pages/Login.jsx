0b3312b47e11b399a7fdf7af39847c2cf010aaf0
Alejandro Nambo <alejandronambo24@gmail.com>
2025-04-30 23:03:26 -0700

    Edit to the OTP redirection after login attempt

--- StudyHive/Frontend/studyhive/src/pages/Login.jsx
+++ StudyHive/Frontend/studyhive/src/pages/Login.jsx
@@ -73,7 +73,7 @@ export default function Login() {
               position: "top-center",
               autoClose: 1500,
             });
-            navigate("/otp", { state: { email: formData.email } });
+            navigate("/verify-otp", { state: { email: formData.email } });
 
             return;
           }
